82bf64187bb2c25d5162e2ef8aa8561abac5156a
jencko <ashishsinghbardhan@gmail.com>
2018-02-26 05:44:57 +0530

    delete django project and re-organized the project to html css js

--- check/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class CheckConfig(AppConfig):
-    name = 'check'
--- check/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index,name="index"),
-    url(r'^weather-today/', views.weather_today,name="weather_today"),
-]
